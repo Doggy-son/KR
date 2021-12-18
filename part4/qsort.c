ff162c6e65b02d5b4c4ddabecd4a7f141643c413
Doggy-son <lukychka@gmail.com>
2021-12-19 00:49:56 +0300

    алгоритм быстрой сортировки

--- part4/qsort.c
+++ part4/qsort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*быстрая сортировка с использованием рекурсии
 сортирует v[left]..v[right] по возрастанию*/
+#define DEBUG 1
 
 void qsort(int v[], int left, int right)
 {
@@ -13,11 +14,11 @@ void qsort(int v[], int left, int right)
   swap(v, left, (left + right)/2); // элемент из середины переносится в начало
   last = left;
   for (i=left+1; i<=right; i++){
-    if (v[i] < left)
-      swap(v, ++last, i);
+    if (v[i] < v[last])
+      swap(v, last, i);
+      last = i;
     }
-  swap (v, ++last, i);
-  swap (v, left, last);
+    
   qsort(v, left, last-1);
   qsort(v, last+1, right);
 }
@@ -28,14 +29,14 @@ void swap(int v[], int i, int j) //определение функции
 
     temp = v[i];
     v[i] = v[j];
-    v[j] = v[i];
+    v[j] = temp;
 }
 void main(void)
 {
   int i;
   int v[10] = {-1, 4, 9, -23, 15, 2, -10, -11, 9, 0};
 
-  qsort(v, 0, 6);
+  qsort(v, 0, 9);
   for (i=0; i<10; i++)
     printf("%d = %d\n", i, v[i]);
 }
